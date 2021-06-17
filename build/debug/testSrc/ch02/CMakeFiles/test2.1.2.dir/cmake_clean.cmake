8c08a0339b94e2be58d0081f0fa5fa1e8982f762
KorolTung <loveus3512_korol@163.com>
2021-06-17 19:41:06 +0800

    简单的配置了CMakeLists.txt

---
+++ build/debug/testSrc/ch02/CMakeFiles/test2.1.2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/test2.1.2.dir/test2.1.2.cpp.o"
+  "../../bin/test2.1.2.pdb"
+  "../../bin/test2.1.2"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/test2.1.2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
